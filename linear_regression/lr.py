888890dc3a55c9e7fb6e48a8b7f8c780699247e4
Gregory <grihabor@mail.ru>
2017-05-14 00:27:45 +0300

    regression done

--- linear_regression/lr.py
+++ linear_regression/lr.py
@@ -6,7 +6,7 @@ matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
 
-BASE_LR = 0.1
+BASE_LR = 1.5e-4
 DATA_SIZE = 20
 
 ROI_LEFT = -8
@@ -32,18 +32,51 @@ def draw_data(data):
 	plt.savefig('t.png')
 
 
+def draw_lines(line, it):
+	plt.plot([ROI_LEFT, ROI_RIGHT], [line(ROI_LEFT), line(ROI_RIGHT)], 'r')
+	plt.savefig('fig_{}.png'.format(it))
+			 
+
 def main():
 	data = generate_data()
 	draw_data(data)
 
-	k = tf.Variable(tf.constant(0.), name='k')
-	b = tf.Variable(tf.constant(0.), name='b')
-	x = tf.placeholder(tf.float32, name="input_x", shape=[None, 1])
-	y = tf.placeholder(tf.float32, name="groundtrooth_y", shape=[None, 1])
+	k_var = tf.Variable(tf.constant(0.), name='k')
+	b_var = tf.Variable(tf.constant(0.), name='b')
+	x_in = tf.placeholder(tf.float32, name="input_x", shape=[None, 1])
+	y_in = tf.placeholder(tf.float32, name="groundtrooth_y", shape=[None, 1])
 
-	func = tf.multiply(k, x) + b
-	loss = (func - y) ** 2
+	func = tf.multiply(k_var, x_in) + b_var
+	loss = (func - y_in) ** 2
 	optimizer = tf.train.GradientDescentOptimizer(BASE_LR, name='GradientDescent')
+	train_op = optimizer.minimize(loss)
+	
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+
+		for it in range(50):
+			k, b = sess.run([k_var, b_var])
+
+			def line(x):
+				return k * x + b
+
+			if it < 10:
+				draw_lines(line, it)
+
+			def feed_dict():
+				return {
+					x_in: np.reshape(data[:, 0], (-1, 1)), 
+					y_in: np.reshape(data[:, 1], (-1, 1)),
+				}
+
+			sess.run(train_op, feed_dict=feed_dict())
+
+		plt.clf()
+		k, b = sess.run([k_var, b_var])
+		def line(x):
+			return k * x + b
+		draw_data(data)
+		draw_lines(line, 50)
 
 if __name__ == '__main__':
 	main()
